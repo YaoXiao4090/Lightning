d7c9b14c1272d617c7ab0759350e1e7182630db8
YaoXiao4090 <yao.xiao4090@gmail.com>
2024-12-02 20:34:03 -0600

    added pause menu, pause background, and play to start moving background

--- src/project.py
+++ src/project.py
@@ -37,6 +37,14 @@ def scrolling_BG(speed, screen, resolution, Fullscreen):
             screen.blit(BG, (i * BG_width + speed, BG_height * 2))
     return BG_width
 
+def pause_menu(screen, resolution, font_size = 160):
+    text_font = pygame.font.SysFont("None", font_size, bold = True)
+    text_col = pygame.Color(255, 255, 0)
+    text_width, text_height = (resolution[0]/2 - font_size * 1.5, 
+                                 resolution[1]/2 - font_size/2)
+    draw_text(screen, "Paused",
+                text_font, text_col, text_width, text_height)
+    
 def main():
     pygame.init()
     pygame.display.set_caption("Lightning")
@@ -52,7 +60,6 @@ def main():
     run = True
     while run:
         clock.tick(FPS)
-        scroll -= 5
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if Fullscreen == True and event.key == pygame.K_TAB:
@@ -65,14 +72,25 @@ def main():
                     screen = pygame.display.set_mode(resolution)
                     Fullscreen = True
                     pygame.display.toggle_fullscreen()
-                if event.key == pygame.K_ESCAPE:
+                elif event.key == pygame.K_ESCAPE:
                     run = False
+                elif Play == False and event.key == pygame.K_RETURN:
+                    Play = True
+                elif Pause == False and event.key == pygame.K_SPACE:
+                    Pause = True
+                elif event.key == pygame.K_SPACE:
+                    Pause = False
             if event.type == pygame.QUIT:
                 run = False
+        if Play == True and Pause == False:
+            scroll -= 5
         BG_width = scrolling_BG(scroll, screen, resolution, Fullscreen)
         if abs(scroll) > BG_width:
             scroll = 0
-        menu_text(screen, resolution, font_size = 40, title_size = 80)
+        if Play == False:
+            menu_text(screen, resolution, font_size = 40, title_size = 80)
+        if Play == True and Pause == True:
+            pause_menu(screen, resolution, font_size = 160)
         pygame.display.update()
         
     pygame.quit()
